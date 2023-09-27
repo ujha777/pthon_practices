c526837d74cb2a9f196d07ed3cec69eebe9662dc
ujha777 <ujha777@gmail.com>
2023-09-28 00:32:43 +0530

    First Python Program

---
+++ Practice/FirstDemo.py
@@ -0,0 +1,18 @@
+print("hello")
+# here are the comments  I have defined
+i = 10
+print(i)
+
+Str = "Hello world"
+print(str)
+
+b, c, d = 5, 6.4, "Great"
+print("{} {}".format("Value is", b))
+
+print("{} {}".format("Value is", c))
+
+print("{} {}".format("Value is", d))
+
+print(type(b))
+print(type(c))
+print(type(d))
